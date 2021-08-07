788b9b81c8638b5fadaf28d087b9467a9a891740
zoneam <zoneam@gmail.com>
2021-08-06 19:59:20 -0700

    changed file structure
    separated api routes and html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+
+const Workout = require('../models/workout');
+
+router.post('/api/workouts', async ({ body }, res) => {
+  try {
+      const workout = await Workout.create(body);
+      res.json(workout);
+  } catch (error) {
+      res.json(error)
+  }
+});
+
+router.put('/api/workouts/:id', async ({ body, params }, res) => {
+  try {
+      const workoutData = await Workout.updateOne({
+          _id: params.id,
+      },
+      {
+          $inc: { totalDuration: body.duration },
+          $push: { exercises: body }
+      },
+      { new: true });
+    res.json(workoutData);
+  }
+  catch (error) {
+      res.json(error)
+  }
+});
+
+router.get('/api/workouts', async (req, res) => {
+  try{
+      
+      const workoutData = await Workout.aggregate([
+          {$addFields: {
+              totalDuration: {$sum: '$exercises.duration'}
+          }}
+      ]);
+      res.json(workoutData);
+
+  }
+  catch (error) {
+      res.json(error)
+  }
+});
+
+router.get('/api/workouts/range', async (req, res) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: '$exercises.duration' }
+        }
+      }
+    ]).sort({day: -1}).limit(7).sort({day: 1});
+     res.json(workouts) 
+  } catch (error) {
+      res.json(error)
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
-
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
@@ -13,80 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/stats.html"));
-});
-
-app.get('/api/workouts', async (req, res) => {
-  try{
-      
-      const workoutData = await db.Workout.aggregate([
-          {$addFields: {
-              totalDuration: {$sum: '$exercises.duration'}
-          }}
-      ]);
-      res.json(workoutData);
-
-  }
-  catch (error) {
-      res.json(error)
-  }
-});
-
-app.post('/api/workouts', async ({ body }, res) => {
-  try {
-      const workout = await db.Workout.create(body);
-      res.json(workout);
-  } catch (error) {
-      res.json(error)
-  }
-});
-
-app.put('/api/workouts/:id', async ({ body, params }, res) => {
-  try {
-      const workoutData = await db.Workout.updateOne({
-          _id: params.id,
-      },
-      {
-          $inc: { totalDuration: body.duration },
-          $push: { exercises: body }
-      },
-      { new: true });
-      res.json(workoutData);
-  }
-  catch (error) {
-      res.json(error)
-  }
-});
-
-app.get('/api/workouts/range', async (req, res) => {
-  try {
-    const workouts = await db.Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: { $sum: '$exercises.duration' }
-        }
-      }
-    ]).sort({day: -1}).limit(7).sort({day: 1});
-     res.json(workouts) 
-  } catch (error) {
-      res.json(error)
-  }
 });
 
 app.listen(PORT, () => {
